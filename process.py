1a170f7f481e2aebc67fed77db9930c6a110cf8f
Exr0n <mail@exr0n.com>
2020-09-06 19:27:44 -0700

    radius over prefix

--- process.py
+++ process.py
@@ -1,4 +1,4 @@
-ROLLING_DISTANCE = 75
+ROLLING_RADIUS = 30
 
 from matplotlib import pyplot as plt
 from re import sub
@@ -13,12 +13,12 @@ text = text.split(' ')                                                          
 text = list(filter(lambda w: len(w), text))                                     # remove empty "words"
 paragraphs = [1]+[i for i,v in enumerate(text) if v[-1] == '\n']                # get paragraph indices by word number
 percent_of_prev_fifty = [                                                       # calculate statistics:
-        len(list(filter(lambda w: w in ['siriak', 'he', 'his'],                 # count number of "valid" words
-        text[max(i-ROLLING_DISTANCE, 0):i])))                                   # of the previous ROLLING_DISTANCE words
-        /ROLLING_DISTANCE *100 for i in range(len(text))]                       # converted to percent, for every word index
+        len(list(filter(lambda w: w in ['siriak', 'he', 'his', 'him'],          # count number of "valid" words
+        text[max(i-ROLLING_RADIUS, 0):min(i+ROLLING_RADIUS, len(text))])))      # of the surrounding 2*ROLLING_RADIUS words
+        /ROLLING_RADIUS *50 for i in range(len(text))]                          # converted to percent, for every word index
 plt.style.use('dark_background')                                                # dark mode
 plt.plot(percent_of_prev_fifty)                                                 # plot rolling average
-plt.ylabel(f'References to Siriak in previous {ROLLING_DISTANCE} words')        # set axis labels
+plt.ylabel(f'References to Siriak in nearby {ROLLING_RADIUS*2} words')          # set axis labels
 plt.xlabel('Paragraph number')
 plt.xticks(paragraphs, range(1, len(paragraphs)+1))                             # show paragraph markers
 plt.savefig('chart', dpi=300)                                                   # save as image
